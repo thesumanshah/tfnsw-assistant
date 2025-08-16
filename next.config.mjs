49388a3c2d605bec62ecdd2877beb84c364b7623
suman shah <sumanshahofficial@gmail.com>
2025-08-16 19:49:43 +1000

    🔧 Simplify next-pwa options to supported minimal set (dest, disable) to resolve GenerateSW config errors

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  }
 });
 
 /** @type {import('next').NextConfig} */
